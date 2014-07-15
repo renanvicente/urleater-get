861ddb9e7e9a0e539423092156d4a7121818ad16
Renan Vicente <renanvice@gmail.com>
2014-07-15 10:23:47 -0300

    changed initscripts by distro

--- install_nix.sh
+++ install_nix.sh
@@ -3,10 +3,13 @@ DIR=$(dirname "${BASH_SOURCE[0]}")
 mkdir -p /usr/local/bin/urleater-get
 cp $DIR/src/urleater-get.py /usr/local/bin/urleater-get/
 cp $DIR/src/urleater-get.conf /etc/
-cp $DIR/initscripts/* /etc/init.d/
 if [ -f /etc/debian_version ];then
+  cp $DIR/initscripts/urleater-get-debian /etc/init.d/
   update-rc.d -n urleater-get defaults
 elif [ -f /etc/redhat-version ];then
+  cp $DIR/initscripts/urleater-get-redhat /etc/init.d/
   chkconfig --add urleater-get 
   chkconfig urleater on
+else
+  echo 'System not supported'
 fi
