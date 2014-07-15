e2bb7ab71fb7a206d87c5c9965389d3257bca814
Renan Vicente <renanvice@gmail.com>
2014-07-15 11:39:06 -0300

    Update install_nix.sh

--- install_nix.sh
+++ install_nix.sh
@@ -4,10 +4,10 @@ mkdir -p /usr/local/bin/urleater-get
 cp $DIR/src/urleater-get.py /usr/local/bin/urleater-get/
 cp $DIR/src/urleater-get.conf /etc/
 if [ -f /etc/debian_version ];then
-  cp $DIR/initscripts/urleater-get-debian /etc/init.d/
+  cp $DIR/initscripts/urleater-get-debian /etc/init.d/urleater-get
   update-rc.d -n urleater-get defaults
 elif [ -f /etc/redhat-version ];then
-  cp $DIR/initscripts/urleater-get-redhat /etc/init.d/
+  cp $DIR/initscripts/urleater-get-redhat /etc/init.d/urleater-get
   chkconfig --add urleater-get 
   chkconfig urleater on
 else
